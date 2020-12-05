6e47e624b02e4efb363e37e689814f7d450e4ba0
MoslenOK <sututov69@gmail.com>
2020-12-05 12:54:20 +0300

    Initial Commit

---
+++ SetviceHours/SetviceHoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetviceHoursApp.swift
+//  SetviceHours
+//
+//  Created by DENIS SYTYTOV on 05.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SetviceHoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
